24883527893ef60b9988bd613dc19258af6c8e89
lashasaata <lashasaata@gmail.com>
2024-03-16 14:08:18 -0700

    fixed background size

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Result from "./components/Result";
 function App() {
   const [useSubmit, setUseSubmit] = useState(false);
   return (
-    <div className="min-h-screen flex flex-col items-center bg-white relative">
-      <div className="h-[240px] min-w-full bg-[url('./images/bg-main-mobile.png')] bg-no-repeat bg-cover"></div>
-      <div className="flex flex-col items-center gap-[142px] absolute">
+    <div className="min-h-screen flex flex-col lg:flex-row items-center lg:justify-center bg-white relative">
+      <div className="min-h-[240px] lg:min-h-screen min-w-full lg:min-w-0 lg:w-2/6 lg:mr-auto bg-[url('./images/bg-main-mobile.png')] lg:bg-[url('./images/bg-main-desktop.png')] bg-no-repeat bg-cover"></div>
+      <div className="flex flex-col lg:flex-row items-center gap-[142px] lg:gap-[127px] absolute">
         <Cards />
         {!useSubmit ? (
           <Register useSubmit={useSubmit} setUseSubmit={setUseSubmit} />

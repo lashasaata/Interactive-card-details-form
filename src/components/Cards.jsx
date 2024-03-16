559f84bc203c8185cfcd344834f5b83ef1ba6a35
lashasaata <lashasaata@gmail.com>
2024-03-16 14:05:44 -0700

    large styles for card component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,22 +1,26 @@
 function Cards() {
   return (
-    <div className="w-[343px] flex justify-end mt-[32px] relative">
-      <div className="bg-[url('././images/bg-card-back.png')] w-[286px] h-[157px] bg-cover flex items-center rounded-[6px]">
-        <span className="text-white text-[9px] font-[500] tracking-[1.29px] ml-[229px]">
+    <div className="w-[343px] lg:w-[541px] lg:h-[527px] flex justify-end lg:items-end mt-[32px] lg:mt-[0px] relative">
+      <div className="bg-[url('././images/bg-card-back.png')] w-[286px] lg:w-[447px] h-[157px] lg:h-[245px] bg-cover flex items-center rounded-[6px]">
+        <span className="text-white text-[9px] lg:text-sm font-[500] tracking-[1.29px] lg:tracking-[2px] ml-[229px] lg:ml-[358px]">
           000
         </span>
       </div>
-      <div className="bg-[url('././images/bg-card-front.png')] w-[285px] bg-cover pt-[17.5px] pl-[19px] pb-[19px] rounded-[6px] absolute left-[0px] top-[89px]">
-        <img className="h-[30px]" src="././images/card-logo.svg" alt="logo" />
-        <div className="flex flex-col gap-[17px] mt-[37px]">
-          <span className="text-lg text-white font-[500] tracking-[2.2px]">
+      <div className="bg-[url('././images/bg-card-front.png')] w-[285px] lg:w-[447px] bg-cover pt-[17.5px] lg:pt-[28px] pl-[19px] lg:pl-[32px] pb-[19px] lg:pb-[26px ] rounded-[6px] lg:rounded-[10px] absolute left-[0px] top-[89px] lg:top-[0px]">
+        <img
+          className="h-[30px] lg:h-[47px]"
+          src="././images/card-logo.svg"
+          alt="logo"
+        />
+        <div className="flex flex-col gap-[17px] lg:gap-[25.5px] mt-[37px] lg:mt-[64px]">
+          <span className="text-lg lg:text-[28px] text-white font-[500] tracking-[2.2px] lg:tracking-[3.42px]">
             0000 0000 0000 0000
           </span>
-          <div className="w-[245px] flex items-center justify-between">
-            <span className="text-[9px] text-white font-[500] tracking-[1.29px]">
+          <div className="w-[245px] lg:w-[382px] flex items-center justify-between">
+            <span className="text-[9px] lg:text-sm text-white font-[500] tracking-[1.29px] lg:tracking-[2px]">
               00
             </span>
-            <span className="text-[9px] text-white font-[500] tracking-[1.29px]">
+            <span className="text-[9px] lg:text-sm text-white font-[500] tracking-[1.29px] lg:tracking-[2px]">
               0000
             </span>
           </div>

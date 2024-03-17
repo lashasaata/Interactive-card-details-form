054169ad136e792b942c40592f9c3706c7ba542e
lashasaata <lashasaata@gmail.com>
2024-03-17 07:55:02 -0700

    install reactinputmask and make some changes in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,27 @@ import Register from "./components/Register";
 import Result from "./components/Result";
 function App() {
   const [useSubmit, setUseSubmit] = useState(false);
+  const [useInputValue, setUseInputValue] = useState({
+    userName: "",
+    cardNumber: "",
+    month: "",
+    year: "",
+    cvc: "",
+  });
+
   return (
     <div className="min-h-screen flex flex-col lg:flex-row items-center lg:justify-center bg-white relative">
       <div className="min-h-[240px] lg:min-h-screen min-w-full lg:min-w-0 lg:w-2/6 lg:mr-auto bg-[url('./images/bg-main-mobile.png')] lg:bg-[url('./images/bg-main-desktop.png')] bg-no-repeat bg-cover"></div>
       <div className="flex flex-col lg:flex-row items-center gap-[142px] lg:gap-[127px] absolute">
-        <Cards />
+        <Cards useInputValue={useInputValue} />
         {!useSubmit ? (
-          <Register useSubmit={useSubmit} setUseSubmit={setUseSubmit} />
+          <Register
+            setUseSubmit={setUseSubmit}
+            useInputValue={useInputValue}
+            setUseInputValue={setUseInputValue}
+          />
         ) : (
-          <Result useSubmit={useSubmit} setUseSubmit={setUseSubmit} />
+          <Result setUseSubmit={setUseSubmit} />
         )}
       </div>
     </div>

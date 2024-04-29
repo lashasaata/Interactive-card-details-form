ad4a1ccc26fc27bf5d1028169ead5e649a9dd33c
lashasaata <lashasaata@gmail.com>
2024-04-29 03:29:04 -0700

    fix background imgs

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="min-h-screen flex flex-col lg:flex-row items-center lg:justify-center bg-white relative">
-      <div className="min-h-[240px] lg:min-h-screen min-w-full lg:min-w-0 lg:w-2/6 lg:mr-auto bg-[url('./images/bg-main-mobile.png')] lg:bg-[url('./images/bg-main-desktop.png')] bg-no-repeat bg-cover"></div>
+      <div className="min-h-[240px] lg:min-h-screen min-w-full lg:min-w-0 lg:w-2/6 lg:mr-auto bg-[url('/images/bg-main-mobile.png')] lg:bg-[url('/images/bg-main-desktop.png')] bg-no-repeat bg-cover"></div>
       <div className="flex flex-col lg:flex-row items-center gap-[142px] lg:gap-[127px] absolute">
         <Cards useInputValue={useInputValue} />
         {!useSubmit ? (
